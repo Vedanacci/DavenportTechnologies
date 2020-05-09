07b37421d548c140b8edc7fa4777db0c739f343c
Vedanacci <bahad16042@gapps.uwcsea.edu.sg>
2020-05-09 10:20:51 +0800

    Initial Commit

---
+++ CodeForCoronaBaseFile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeForCoronaBaseFile
+//
+//  Created by Vedant Bahadur on 9/5/20.
+//  Copyright © 2020 Youth Hacks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
