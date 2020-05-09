1bacc89768b0e2480c15dad1fea113a60274fb2b
David Walton <trotsky2022@gmail.com>
2020-05-09 17:37:55 +0800

    Initial Commit

---
+++ codeForCorona/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  codeForCorona
+//
+//  Created by David Walton on 5/9/20.
+//  Copyright © 2020 David Walton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
